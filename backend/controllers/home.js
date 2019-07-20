f0da430bbccfa78c8e5f1d64981db51c3c997a78
Daniel Constantine <danielkevinconstantine@gmail.com>
2019-07-20 17:58:56 -0400

    data

---
+++ backend/controllers/home.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const models = require('../models');
+
+const router = express.Router();
+
+
+router.get("/", (req, res) => {
+  // res.json({
+  //   msg: "Successful GET to '/' route"
+  // });
+
+  res.send(JSON.stringify([{name: "daniel"}, {name:"nerissa"}]));
+});
+
+// router.get('/api', (req, res) => {
+  // res.json({
+  //   msg: "Successful GET to '/' route"
+  // });
+
+//   res.send(JSON.stringify([{name: 'daniel'}, {name:'nerissa'}]));
+// });
+
+router.post('/', (req, res) => {
+  res.json({
+    msg: "Successful POST to '/' route"
+  });
+});
+
+router.put('/:id', (req, res) => {
+  res.json({
+    msg: "Successful PUT to '/' route",
+    id: req.params.id
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  res.json({
+    msg: "Successful DELETE to '/' route",
+    id: req.params.id
+  });
+});
+
+
+module.exports = router;
